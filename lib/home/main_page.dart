c4d71470554ed5ee1c8390dee47219300233fc9e
Alex <alexawollo@gmail.com>
2023-01-03 21:26:28 +0100

    main_page

---
+++ lib/home/main_page.dart
@@ -0,0 +1,52 @@
+import 'package:app_pandoro/utils/colors.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+
+            child: Container(
+              margin: EdgeInsets.only(top: 45, bottom: 15),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Text("Country"),
+                      Text("City")
+                    ],
+                  ),
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+
+                      ),
+                      child: const Icon(Icons.search, color: Colors.white)
+                    ),
+                  )
+                ],
+              ),
+            )
+          ) // no sé si aquí va una coma o no
+        ],
+      )
+    ); //
+  }
+}
